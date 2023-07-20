3c32040b235f18e56d26d81868a2ccbdf1085bdf
Alexandra Haynes <sandamunteanua@yahoo.com>
2023-07-19 16:57:54 -1000

    add footer

---
+++ src/app/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import Logo from "../assets/images/Logo.png";
+
+const Footer = () => (
+  <div className="mt-80px bg-pink-100">
+    <div className="flex flex-col items-center px-40px pt-24px gap-40px flex-wrap">
+      {/* <img src={Logo} alt="logo" className="w-200px h-41px" /> */}
+    Logo
+    </div>
+    <h5 className="text-4xl xs:text-20px text-center mt-41px pb-40px">
+      Copyright My Workouts
+    </h5>
+  </div>
+);
+
+export default Footer;
