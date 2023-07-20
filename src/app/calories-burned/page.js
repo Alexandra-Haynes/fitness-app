bbccbf81aee9bca66a2a0d8b894ec04c8be7dc2d
Alexandra Haynes <sandamunteanua@yahoo.com>
2023-07-20 10:01:47 -1000

    add calorie calculator page

---
+++ src/app/calories-burned/page.js
@@ -0,0 +1,31 @@
+import React from "react";
+import CalorieCalculatorForm from "../components/CalorieCalculatorForm";
+import Image from "next/image";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
+
+const CaloriesBurned = () => {
+  return (
+    <>
+      <div className="h-fit min-h-screen relative overflow-x-hidden">
+        <Navbar />
+        <h1 className="text-3xl font-light py-6 text-center text-primary">
+          Calorie Calculator
+        </h1>
+
+        <Image
+          src="/assets/dumbell.png"
+          width={320}
+          height={180}
+          className="absolute bottom-4 -right-24 -z-10"
+        />
+        <CalorieCalculatorForm />
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default CaloriesBurned;
---
+++ src/app/components/CalorieCalculatorForm.js
@@ -0,0 +1,167 @@
+"use client";
+
+import React, { useState } from "react";
+import axios from "axios";
+import Image from "next/image";
+
+const CalorieCalculatorForm = () => {
+  const [activityName, setActivityName] = useState("");
+  const [weight, setWeight] = useState(125);
+  const [duration, setDuration] = useState(30);
+  const [activities, setActivities] = useState([]);
+  const [activityNotFound, setActivityNotFound] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "activityName") {
+      setActivityName(value);
+    } else if (name === "weight") {
+      setWeight(value);
+    } else if (name === "duration") {
+      setDuration(value);
+    }
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.get(
+        "https://calories-burned-by-api-ninjas.p.rapidapi.com/v1/caloriesburned",
+        {
+          params: {
+            activity: activityName,
+            weight: weight,
+            duration: duration,
+          },
+          headers: {
+            "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+            "X-RapidAPI-Host": "calories-burned-by-api-ninjas.p.rapidapi.com",
+          },
+        }
+      );
+
+      if (response.data.length > 0) {
+        setActivities(response.data);
+        setActivityNotFound(false);
+      } else {
+        setActivities([]);
+        setActivityNotFound(true);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={handleFormSubmit}
+        className="bg-white border w-[90%] mx-auto 
+      flex flex-col items-start justify-center gap-6
+      rounded-md shadow-xl p-8 max-w-[800px]"
+      >
+        <label className="font-semibold pr-2">
+          Activity Name:
+          <input
+            type="text"
+            name="activityName"
+            value={activityName}
+            onChange={handleInputChange}
+            className="text-xl font-mono my-2 text-black 
+            border-2 border-highlights  p-2 rounded-sm"
+          />
+        </label>
+        <label className="font-semibold">
+          Weight
+          <input
+            type="number"
+            name="weight"
+            value={weight}
+            onChange={handleInputChange}
+            placeholder="Enter your weight"
+            className="text-xl ml-2 font-mono w-[80px] text-center
+             focus:bg-highlights bg-highlights/80 py-1 rounded-full"
+          />{" "}
+          lbs
+        </label>
+        <label className="font-semibold">
+          Workout duration
+          <input
+            type="number"
+            name="duration"
+            value={duration}
+            onChange={handleInputChange}
+            placeholder="Enter the duration"
+            className="text-xl ml-2 font-mono w-[80px] text-center
+             focus:bg-highlights bg-highlights/80 py-1 rounded-full"
+          />{" "}
+          min
+        </label>
+        <button
+          type="submit"
+          className="bg-primary p-4 rounded-full shadow-xl text-white text-center
+        hover:bg-primary/80 hover:translate-y-1 px-8
+        "
+        >
+          Calculate Calories
+        </button>
+      </form>
+
+      {activityNotFound && (
+        <div
+          className="w-[80%] border border-dashed p-12 bg-white/30
+        flex flex-row gap-2 items-center justify-center pt-4 mx-auto mt-12"
+        >
+          <Image
+            src="/assets/no-results.png"
+            width={80}
+            height={80}
+            className=""
+          />
+          <p
+            className="w-[80%]  text-center  
+        "
+          >
+            We couldn't find this activity. Try a different version. For
+            example: run instead of running, pushups instead of push ups or
+            push-ups
+          </p>
+        </div>
+      )}
+      {/* Display activity details */}
+      {activities.length > 0 && (
+        <div
+          className="bg-white border w-[90%] max-w-[800px] mx-auto 
+  rounded-md shadow-xl p-6 my-12"
+        >
+          <table className="w-full text-center ">
+            <thead>
+              <tr className="border-b-2 border-b-highlights">
+                <th className="py-3 pl-2 text-left">Activity Name</th>
+                <th className="py-3">Calories per hour</th>
+                <th className="py-3">Total calories</th>
+              </tr>
+            </thead>
+            <tbody>
+              {activities.map((activity, index) => (
+                <tr
+                  key={index}
+                  className={index % 2 === 0 ? "" : "bg-primary/20"}
+                >
+                  <td className="py-3 pl-2 font-semibold text-primary text-left">
+                    {activity.name}
+                  </td>
+                  <td className="py-3">{activity.calories_per_hour}</td>
+                  <td className="py-3">{activity.total_calories}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CalorieCalculatorForm;
