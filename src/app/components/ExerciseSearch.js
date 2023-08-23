aaf80f8aa6b5b2e3c9b08781b714980934654601
Alexandra Haynes <sandamunteanua@yahoo.com>
2023-08-23 12:39:54 -1000

    add exercise filtering

--- src/app/components/ExerciseSearch.js
+++ src/app/components/ExerciseSearch.js
@@ -1,27 +1,54 @@
 import React, { useState } from "react";
-import axios from "axios";
 import ExerciseCard from "./ExerciseCard";
 import FrequentExercises from "./FrequentExercises";
 import Image from "next/image";
+import exercisesData from "../data/exercises.json";
 import { CiSearch } from "react-icons/ci";
-import { BsCheck } from "react-icons/bs";
-import { IoMdClose } from "react-icons/io";
-import {AiFillCaretRight, AiFillCaretLeft} from 'react-icons/ai'
+import { AiFillCaretRight, AiFillCaretLeft } from "react-icons/ai";
+import SearchExercisesFilter from "./SearchExercisesFilter";
+
+const categories = [
+  { id: "barbell", label: "Barbell" },
+  { id: "dumbbells", label: "Dumbbells" },
+  { id: "kettlebells", label: "Kettlebells" },
+  { id: "stretches", label: "Stretches" },
+  { id: "cables", label: "Cables" },
+  { id: "band", label: "Bands" },
+  { id: "plate", label: "Plates" },
+  { id: "trx", label: "TRX" },
+  { id: "bodyweight", label: "Bodyweight" },
+  { id: "yoga", label: "Yoga" },
+  { id: "machine", label: "Machine" },
+];
+
+const muscles = [
+  "Biceps",
+  "Forearms",
+  "Shoulders",
+  "Triceps",
+  "Quads",
+  "Glutes",
+  "Lats",
+  "Mid back",
+  "Lower back",
+  "Hamstrings",
+  "Chest",
+  "Abdominals",
+  "Obliques",
+  "Traps",
+  "Calves",
+];
 
 const ExerciseSearch = () => {
   const [searchedEx, setSearchedEx] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [showSuggestions, setShowSuggestions] = useState(false);
-
   const [selectedSuggestion, setSelectedSuggestion] = useState("");
-
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [selectedDifficulty, setSelectedDifficulty] = useState("");
-  const [selectedMuscle, setSelectedMuscle] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedDifficulty, setSelectedDifficulty] = useState(null);
+  const [selectedMuscle, setSelectedMuscle] = useState(null);
   const [exercises, setExercises] = useState([]); //exercise_name output
-
   const [searchClicked, setSearchClicked] = useState(false);
-
   const [numOfResults, setNumOfResults] = useState(0);
   const [filterIsOpen, setFilterIsOpen] = useState(false);
   const [isRocketAnimating, setRocketAnimating] = useState(false);
@@ -31,82 +58,33 @@ const ExerciseSearch = () => {
   const itemsPerPage = 12;
   const [currentPage, setCurrentPage] = useState(1);
 
-  const categories = [
-    { id: "barbell", label: "Barbell" },
-    { id: "dumbbells", label: "Dumbbells" },
-    { id: "kettlebells", label: "Kettlebells" },
-    { id: "stretches", label: "Stretches" },
-    { id: "cables", label: "Cables" },
-    { id: "band", label: "Bands" },
-    { id: "plate", label: "Plates" },
-    { id: "trx", label: "TRX" },
-    { id: "bodyweight", label: "Bodyweight" },
-    { id: "yoga", label: "Yoga" },
-    { id: "machine", label: "Machine" },
-  ];
-  const muscles = [
-    "Biceps",
-    "Forearms",
-    "Shoulders",
-    "Triceps",
-    "Quads",
-    "Glutes",
-    "Lats",
-    "Mid back",
-    "Lower back",
-    "Hamstrings",
-    "Chest",
-    "Abdominals",
-    "Obliques",
-    "Traps",
-    "Calves",
-  ];
-
   const handleMuscleSelection = (muscle) => {
     setSelectedMuscle(muscle);
+    console.log('muscle selected', muscle)
   };
 
   const handleOpenFilters = () => {
     return setFilterIsOpen(true);
   };
   const handleClearFilters = () => {
-    setSelectedCategory("");
-    setSelectedDifficulty("");
-    setSelectedMuscle("");
-  };
-
-  const handleCloseFilters = () => {
-    setFilterIsOpen(false);
+    setSelectedCategory(null);
+    setSelectedDifficulty(null);
+    setSelectedMuscle(null);
   };
 
   const handleSearchClick = () => {
     setSearchClicked(true);
   };
 
-  const fetchAutocompleteSuggestions = async (input) => {
+  const fetchAutocompleteSuggestions = (input) => {
     try {
-      const response = await axios.get(
-        "https://musclewiki.p.rapidapi.com/exercises",
-        {
-          params: {
-            name: input,
-          },
-          headers: {
-            "X-RapidAPI-Key":
-              "7fe9b1ed76msha35f8532c12af1fp1a55d7jsncc554e99d95b",
-            "X-RapidAPI-Host": "musclewiki.p.rapidapi.com",
-          },
-        }
-      );
-
-      const filteredExerciseNames = response.data.filter((exercise) =>
+      const filteredExerciseNames = exercisesData.filter((exercise) =>
         exercise.exercise_name.toLowerCase().includes(input.toLowerCase())
       );
 
       let suggestionsWithCloseButton = filteredExerciseNames;
 
       if (input.trim() !== "") {
-        // Add the "Close Suggestions" option when the input is not empty
         suggestionsWithCloseButton = [
           { exercise_name: "Close Suggestions" },
           ...filteredExerciseNames,
@@ -123,12 +101,12 @@ const ExerciseSearch = () => {
     const { name, value } = event.target;
     if (name === "searchedEx") {
       setSearchedEx(value);
-      setSelectedSuggestion(""); // Clear the selected suggestion when typing in the search bar
+      setSelectedSuggestion("");
       if (value.trim() === "") {
-        setShowSuggestions(false); // Hide the suggestions when the search bar is empty
+        setShowSuggestions(false);
       } else {
-        setShowSuggestions(true); // Show the suggestion dropdown when typing
-        fetchAutocompleteSuggestions(value); // Fetch suggestions based on the current input value
+        setShowSuggestions(true);
+        fetchAutocompleteSuggestions(value);
       }
     }
   };
@@ -142,60 +120,82 @@ const ExerciseSearch = () => {
     }
   };
 
-  const fetchAllExercises = async () => {
-    try {
-      const response = await axios.get(
-        "https://musclewiki.p.rapidapi.com/exercises",
-        {
-          params: {
-            name: "", // Set name as an empty string to get all exercises
-          },
-          headers: {
-            "X-RapidAPI-Key":
-              "7fe9b1ed76msha35f8532c12af1fp1a55d7jsncc554e99d95b",
-            "X-RapidAPI-Host": "musclewiki.p.rapidapi.com",
-          },
-        }
-      );
+  const filterByName = (exercises, name) => {
+    console.log("filtering by name...");
+    return exercises.filter((exercise) =>
+      exercise.exercise_name.toLowerCase().includes(name.toLowerCase())
+    );
+  };
 
-      // Filter exercises to match the current search input
-      const filteredExercises = response.data.filter((exercise) =>
-        exercise.exercise_name.toLowerCase().includes(searchedEx.toLowerCase())
+  const filterByCategory = (exercises, category) => {
+    console.log("filtering by category...");
+    return category
+      ? exercises.filter((exercise) => exercise.Category.toLowerCase() === category.toLowerCase())
+      : exercises;
+  };
+
+  const filterByDifficulty = (exercises, difficulty) => {
+    console.log("filtering by difficulty...");
+    return difficulty
+      ? exercises.filter((exercise) => exercise.Difficulty === difficulty)
+      : exercises;
+  };
+
+  const filterByMuscle = (exercises, muscle) => {
+    console.log("filtering by muscle...");
+    return muscle
+      ? exercises.filter(
+          (exercise) =>
+            exercise.target &&
+            exercise.target.Primary &&
+            exercise.target.Primary.length > 0 &&
+            exercise.target.Primary[0] === muscle
+        )
+      : exercises;
+  };
+
+  const fetchExercises = (e) => {
+    handleSearchClick();
+    handleRocketTakeoff();
+    e.preventDefault();
+
+    try {
+      let filteredExercises = [...exercisesData];
+      console.log(filteredExercises);
+
+      filteredExercises = filterByName(filteredExercises, searchedEx);
+      filteredExercises = filterByCategory(filteredExercises, selectedCategory);
+      filteredExercises = filterByDifficulty(
+        filteredExercises,
+        selectedDifficulty
       );
+      filteredExercises = filterByMuscle(filteredExercises, selectedMuscle);
 
       setExercises(filteredExercises);
+      setNumOfResults(filteredExercises.length);
+      setCurrentPage(1);
+
+      console.log(exercises);
     } catch (error) {
       console.error(error);
     }
   };
 
-  const fetchExercises = async (e) => {
-    handleSearchClick();
-    handleRocketTakeoff();
-    e.preventDefault();
-    let url = "https://musclewiki.p.rapidapi.com/exercises";
-    try {
-      const response = await axios.get(url, {
-        params: {
-          name: searchedEx,
-          category: selectedCategory,
-          difficulty: selectedDifficulty,
-          muscle: selectedMuscle,
-        },
-        headers: {
-          "X-RapidAPI-Key":
-            "7fe9b1ed76msha35f8532c12af1fp1a55d7jsncc554e99d95b",
-          // process.env.RAPID_API_KEY,
-          "X-RapidAPI-Host": "musclewiki.p.rapidapi.com",
-        },
-      });
-
-      setExercises(response.data);
-      //   setNumOfResults(exercises.length)
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
+  const resetSearch = () => {
+    setSearchedEx("");
+    setSuggestions([]);
+    setShowSuggestions(false);
+    setSelectedSuggestion("");
+    setSelectedCategory(null);
+    setSelectedDifficulty(null);
+    setSelectedMuscle(null);
+    setExercises([]);
+    setSearchClicked(false);
+    setNumOfResults(0);
+    setFilterIsOpen(false);
+    setCurrentPage(1);
+    setRocketAnimating(false);
+    setDisplayRocket(true);
   };
 
   const handleRocketTakeoff = () => {
@@ -211,15 +211,18 @@ const ExerciseSearch = () => {
   const currentExercises = exercises.slice(startIndex, endIndex);
 
   return (
-    <section className="relative h-fit md:w-[80%] pt-12  mx-auto flex flex-col items-center justify-center">
-      <h1 className="text-gray-200 text-4xl text-center py-12 pt-24">
+    <section
+      className="relative min-h-screen h-fit md:w-[80%] mx-auto
+     flex flex-col items-center justify-start pt-24 pb-24"
+    >
+      <h1 className="text-gray-200 text-4xl text-center py-12 ">
         Search exercises
       </h1>
       <div
-        className="w-[80%] flex flex-col-reverse md:flex-row-reverse items-center justify-center 
-       px-2 "
+        className="w-[80%] flex flex-col-reverse md:flex-row-reverse
+         items-center justify-center px-2 "
       >
-        {/* _________________________________________________SEARCH BAR___________________________________ */}
+        {/* _____________________SEARCH BAR___________________________________ */}
 
         <div className=" ">
           <div className="flex flex-row items-center justify-start gap-4">
@@ -281,134 +284,56 @@ const ExerciseSearch = () => {
           </div>
 
           {filterIsOpen && (
-            <div className="flex flex-col items-start mt-4 gap-2 ">
-              {/* _________________________________________________DIFFICULTY___________________________________ */}
-
-              <div className="w-full flex flex-row items-center justify-between">
-                <div className=" flex flex-row gap-2 items-center justify-center py-4 text-slate-300">
-                  Difficulty level:{" "}
-                  <div
-                    onClick={() => setSelectedDifficulty("beginner")}
-                    className={`h-6 w-6 rounded-full bg-green-500  hover:scale-105 cursor-pointer
-                  flex items-center justify-center${
-                    selectedDifficulty === "beginner" ? "border-black " : ""
-                  }`}
-                  >
-                    {selectedDifficulty === "beginner" && (
-                      <BsCheck className="text-black text-2xl" />
-                    )}
-                  </div>
-                  <div
-                    onClick={() => setSelectedDifficulty("intermediate")}
-                    className={`h-6 w-6 rounded-full bg-yellow-400 hover:scale-105 cursor-pointer
-                   flex items-center justify-center${
-                     selectedDifficulty === "beginner" ? "border-black " : ""
-                   }`}
-                  >
-                    {selectedDifficulty === "intermediate" && (
-                      <BsCheck className="text-black text-2xl" />
-                    )}
-                  </div>
-                  <div
-                    onClick={() => setSelectedDifficulty("advanced")}
-                    className={`h-6 w-6 rounded-full bg-red-600  hover:scale-105 cursor-pointer
-                  flex items-center justify-center${
-                    selectedDifficulty === "advanced" ? "border-black " : ""
-                  }`}
-                  >
-                    {selectedDifficulty === "advanced" && (
-                      <BsCheck className="text-black text-2xl" />
-                    )}
-                  </div>
-                </div>
-
-                {/* _________________________________________________FILTER BUTTONS___________________________________ */}
-
-                <div className="flex flex-row items-center justify-center gap-4">
-                  {selectedCategory || selectedDifficulty || selectedMuscle ? (
-                    <button
-                      className=" text-slate-400  hover:text-slate-300 underline underline-offset-2"
-                      onClick={handleClearFilters}
-                    >
-                      Clear all
-                    </button>
-                  ) : null}
-                  {filterIsOpen && (
-                    <button
-                      className=" text-slate-400 hover:text-slate-300  underline underline-offset-2"
-                      onClick={handleCloseFilters}
-                    >
-                      <IoMdClose />
-                    </button>
-                  )}
-                </div>
-              </div>
-
-              {/* _________________________________________________CATEGORY___________________________________ */}
-              <div
-                className="max-w-[460px] md:max-w-[800px] grid grid-cols-4
-               lg:grid-cols-6 grid-rows-3 gap-2  text-slate-300"
-              >
-                {" "}
-                Category:
-                {categories.map((category) => (
-                  <div
-                    key={category.id}
-                    onClick={() => setSelectedCategory(category.id)}
-                    className={`text-[.8rem] flex items-center min-w-[80px] max-w-[100px] justify-center text-center rounded cursor-pointer text-black ${
-                      selectedCategory === category.id
-                        ? "  bg-slate-200"
-                        : "bg-slate-400"
-                    }`}
-                  >
-                    {category.label}
-                  </div>
-                ))}
-              </div>
-
-              {/* _________________________________________________MUSCLE___________________________________ */}
-
-              <div className="text-slate-300 mt-4">
-                <p className="col-span-4">Target Muscle:</p>
-                <div
-                  className="z-20 mt-2  max-w-[430px] lg:max-w-[800px]
-              grid grid-cols-4 lg:grid-cols-7 gap-2   "
-                >
-                  {muscles.map((muscle) => (
-                    <div
-                      key={muscle}
-                      onClick={() => handleMuscleSelection(muscle)}
-                      className={`text-[.8rem]  p-1 px-2 rounded  w-[90px] cursor-pointer whitespace-nowrap flex items-center justify-center text-center text-black ${
-                        selectedMuscle === muscle
-                          ? "bg-slate-200"
-                          : "bg-slate-400"
-                      }`}
-                    >
-                      {muscle}
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
+            <SearchExercisesFilter
+              searchedEx={searchedEx}
+              setShowSuggestions={setShowSuggestions}
+              suggestions={suggestions}
+              handleSelect={handleSelect}
+              setSelectedDifficulty={setSelectedDifficulty}
+              selectedDifficulty={selectedDifficulty}
+              handleClearFilters={handleClearFilters}
+              setSelectedCategory={setSelectedCategory}
+              selectedCategory={selectedCategory}
+              categories={categories}
+              handleMuscleSelection={handleMuscleSelection}
+              selectedMuscle={selectedMuscle}
+              muscles={muscles}
+              filterIsOpen={filterIsOpen}
+              setFilterIsOpen={setFilterIsOpen}
+            />
           )}
           {/* _________________________________________________SEARCH BUTTON___________________________________ */}
 
-          <button
-            className="bg-secondary uppercase p-2 px-8 mt-6 rounded-full 
+          <div className="flex flex-row items-center justify-start gap-4 mt-6">
+            <button
+              className="bg-secondary uppercase p-2 px-8 rounded-full 
             hover:translate-y-1 shadow-xl transition-all ease-in-out duration-300"
-            onClick={fetchExercises}
-          >
-            Search
-          </button>
+              onClick={fetchExercises}
+            >
+              Search
+            </button>
+
+            <button className="hover:scale-105" onClick={resetSearch} title='Reset search'>
+              <Image
+                src="/assets/reset.png"
+                width={28}
+                height={28}
+                alt="reset icon"
+              />
+            </button>
+          </div>
         </div>
       </div>
       {/* _________________________________________________RESULTS___________________________________ */}
 
       <ul className="mt-8">
         {currentExercises.map((exercise) => (
-          <ExerciseCard exercise={exercise} />
+          <ExerciseCard key={exercise.id} exercise={exercise} />
         ))}
       </ul>
+      {currentExercises.length === 0 && searchClicked && (
+        <p>No exercises match the criteria.</p>
+      )}
       {/* _________________________________________________FREQ EXERCISE__________________________________ */}
 
       {!searchClicked && <FrequentExercises />}
@@ -432,7 +357,7 @@ const ExerciseSearch = () => {
 
       {/* _________________________________________________PAGINATION___________________________________ */}
 
-      <div className="flex justify-center my-4">
+      <div className="flex justify-center my-4 absolute bottom-0">
         {currentPage > 1 && (
           <button
             className="px-3 py-1  mx-1 my-2   "
---
+++ src/app/components/SearchExercisesFilter.js
@@ -0,0 +1,137 @@
+// FilterComponent.js
+import React from "react";
+import { BsCheck } from "react-icons/bs";
+import { IoMdClose } from "react-icons/io";
+
+const SearchExercisesFilter = ({
+  setSelectedDifficulty,
+  selectedDifficulty,
+  setSelectedCategory,
+  selectedCategory,
+  categories,
+  handleClearFilters,
+  handleMuscleSelection,
+  selectedMuscle,
+  muscles,
+  filterIsOpen,
+  setFilterIsOpen,
+}) => {
+  const handleCloseFilters = () => {
+    setFilterIsOpen(false);
+  };
+
+  return (
+    <div>
+      <div className="flex flex-col items-start mt-4 gap-2 ">
+        {/* _________________________________________________DIFFICULTY___________________________________ */}
+
+        <div className="w-full flex flex-row items-center justify-between">
+          <div className=" flex flex-row gap-2 items-center justify-center py-4 text-slate-300">
+            Difficulty level:{" "}
+            <div
+              onClick={() => setSelectedDifficulty("Beginner")}
+              className={`h-6 w-6 rounded-full bg-green-500  hover:scale-105 cursor-pointer
+                  flex items-center justify-center${
+                    selectedDifficulty === "Beginner" ? "border-black " : ""
+                  }`}
+            >
+              {selectedDifficulty === "Beginner" && (
+                <BsCheck className="text-black text-2xl" />
+              )}
+            </div>
+            <div
+              onClick={() => setSelectedDifficulty("Intermediate")}
+              className={`h-6 w-6 rounded-full bg-yellow-400 hover:scale-105 cursor-pointer
+                   flex items-center justify-center${
+                     selectedDifficulty === "Intermediate" ? "border-black " : ""
+                   }`}
+            >
+              {selectedDifficulty === "Intermediate" && (
+                <BsCheck className="text-black text-2xl" />
+              )}
+            </div>
+            <div
+              onClick={() => setSelectedDifficulty("Advanced")}
+              className={`h-6 w-6 rounded-full bg-red-600  hover:scale-105 cursor-pointer
+                  flex items-center justify-center${
+                    selectedDifficulty === "Advanced" ? "border-black " : ""
+                  }`}
+            >
+              {selectedDifficulty === "Advanced" && (
+                <BsCheck className="text-black text-2xl" />
+              )}
+            </div>
+          </div>
+
+          {/* _________________________________________________FILTER BUTTONS___________________________________ */}
+
+          <div className="flex flex-row items-center justify-center gap-4">
+            {selectedCategory || selectedDifficulty || selectedMuscle ? (
+              <button
+                className=" text-slate-400  hover:text-slate-300 underline underline-offset-2"
+                onClick={handleClearFilters}
+              >
+                Clear all
+              </button>
+            ) : null}
+            {filterIsOpen && (
+              <button
+                className=" text-slate-400 hover:text-slate-300  underline underline-offset-2"
+                onClick={handleCloseFilters}
+              >
+                <IoMdClose />
+              </button>
+            )}
+          </div>
+        </div>
+
+        {/* _________________________________________________CATEGORY___________________________________ */}
+        <div
+          className="max-w-[460px] md:max-w-[800px] grid grid-cols-4
+               lg:grid-cols-6 grid-rows-3 gap-2  text-slate-300"
+        >
+          {" "}
+          Category:
+          {categories.map((category) => (
+            <div
+              key={category.id}
+              onClick={() => setSelectedCategory(category.id)}
+              className={`text-[.8rem] flex items-center min-w-[80px] max-w-[100px] justify-center text-center rounded cursor-pointer text-black ${
+                selectedCategory === category.id
+                  ? "  bg-slate-200"
+                  : "bg-slate-400"
+              }`}
+            >
+              {category.label}
+            </div>
+          ))}
+        </div>
+
+        {/* _________________________________________________MUSCLE___________________________________ */}
+
+        <div className="text-slate-300 mt-4">
+          <p className="col-span-4">Target Muscle:</p>
+          <div
+            className="z-20 mt-2  max-w-[430px] lg:max-w-[800px]
+              grid grid-cols-4 lg:grid-cols-7 gap-2   "
+          >
+            {muscles.map((muscle) => (
+              <div
+                key={muscle}
+                onClick={() => handleMuscleSelection(muscle)}
+                className={`text-[.8rem]  p-1 px-2 rounded  w-[90px] cursor-pointer
+                 whitespace-nowrap flex items-center justify-center text-center text-black ${
+                  selectedMuscle === muscle ? "bg-slate-200" : "bg-slate-400"
+                }`}
+              >
+                {muscle}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchExercisesFilter;
