dacbda13752018aedae950c9287cadd1bd4ca66e
Alexandra Haynes <sandamunteanua@yahoo.com>
2023-08-22 14:58:04 -1000

    hero section styling

--- src/app/components/HeroBanner.js
+++ src/app/components/HeroBanner.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { useSession } from "next-auth/react";
-import React, { useState} from "react";
+import React, { useState } from "react";
+import Image from "next/image";
 import MotivationalQuote from "./MotivationalQuote";
 
 const HeroBanner = ({ navigateToExercises }) => {
@@ -12,13 +13,14 @@ const HeroBanner = ({ navigateToExercises }) => {
   return (
     <>
       <div
-        className={`relative flex flex-col gap-6 items-start
- justify-between max-h-[70%] pt-48 `}
+        className={`relative border-l-2 ml-12 mt-48 border-black flex flex-col gap-6
+         items-start justify-between xl:items-end xl:border-l-0 xl:border-r-2 xl:mr-80 
+        max-h-[70%]  xl:scale-125 xl:ml-0 xl:mt-60`}
       >
         {isLoggedIn ? (
           <div>
             <h2
-              className={`text-white font-semibold text-2xl px-12
+              className={`text-white font-semibold text-2xl px-4
            ${isMounted ? "" : "animate-slide-in-left"}
           `}
             >
@@ -27,7 +29,7 @@ const HeroBanner = ({ navigateToExercises }) => {
           </div>
         ) : (
           <h2
-            className={`text-white font-semibold text-2xl px-12
+            className={`text-white font-semibold text-2xl px-2
            ${isMounted ? "" : "animate-slide-in-left"}
           `}
           >
@@ -36,28 +38,37 @@ const HeroBanner = ({ navigateToExercises }) => {
         )}
 
         <p
-          className={`text-highlights font-bold text-7xl px-12 
+          className={`text-highlights font-bold text-7xl px-2
       ${isMounted ? "" : "animate-slide-in-right"}
       `}
         >
           Get further
-         
         </p>
         <p
-          className={`text-highlights font-bold text-6xl  px-12 
+          className={`text-highlights font-bold text-6xl  px-2
       ${isMounted ? "" : "animate-slide-in-right"}
       `}
         >
           Get ahead
-        
         </p>
 
-        <div
-          className="bg-secondary text-black cursor-pointer hover:translate-y-1 hover:bg-highlights
-        font-semibold mx-12 mt-12 py-3 px-6 text-xl rounded-full
-         transition-all duration-300 animate-slide-in-left"
-        >
-          <Link href="/explore-exercises">Explore exercises </Link>
+        <div className="flex flex-col items-start justify-center gap-4">
+          <div
+            className="bg-white/70 text-black cursor-pointer hover:translate-y-1 hover:bg-highlights
+        font-semibold  mt-12 py-3 px-6 text-lg rounded-sm text-center shadow-xl
+         transition-all duration-300 animate-slide-in-left w-[220px] rounded-r-lg
+          xl:rounded-r-none xl:rounded-l-lg xl:self-end"
+          >
+            <Link href="/explore-exercises">Explore exercises </Link>
+          </div>
+          <div
+            className="bg-white/70 text-black cursor-pointer hover:translate-y-1 hover:bg-highlights
+        font-semibold  py-3 px-6 text-lg rounded-sm text-center shadow-xl
+         transition-all duration-300 animate-slide-in-left w-[190px] rounded-r-lg
+         xl:rounded-r-none xl:rounded-l-lg xl:self-end"
+          >
+            <Link href="/submit-workout">Submit workout </Link>
+          </div>
         </div>
       </div>
       {/* <MotivationalQuote /> */}
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,21 @@
-'use client'
+"use client";
 
-import React, {useState, useEffect} from "react";
-import {  useRouter } from 'next/navigation'; 
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import LoadingPage from "./components/LoadingPage";
 import HeroBanner from "../app/components/HeroBanner";
 
-
 const Home = () => {
-   const router = useRouter();
-     const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
-     //delay home page
-       useEffect(() => {
-         const delay = setTimeout(() => {
-           setIsLoading(false);
-         }, 1200);
-         return () => clearTimeout(delay); // Clear the timeout to avoid memory leaks
-       }, []);
+  //delay home page
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      setIsLoading(false);
+    }, 1200);
+    return () => clearTimeout(delay); // Clear the timeout to avoid memory leaks
+  }, []);
 
   const navigateToExercises = () => {
     router.push("/");
@@ -30,14 +29,11 @@ const Home = () => {
         <>
           <div className=" bg-slate-700 h-screen overflow-x-hidden">
             <div className=" absolute bg-hero opacity-40 w-full h-full
-             overflow-hidden bg-cover bg-center">
-             
-            </div>
+             overflow-hidden bg-cover bg-center"
+            ></div>
 
             <HeroBanner navigateToExercises={navigateToExercises} />
-         
           </div>
-          
         </>
       )}
     </>
