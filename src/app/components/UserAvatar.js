0e839f8dc3750e589433d63b80f80b8f92024362
Alexandra Haynes <sandamunteanua@yahoo.com>
2023-09-05 18:24:02 -1000

    fix avatar resolution

--- src/app/components/UserAvatar.js
+++ src/app/components/UserAvatar.js
@@ -6,8 +6,8 @@ const UserAvatar = ({ avatarUrl = "/assets/avatar/default-avatar.png" }) => {
     <div>
       <Image
         src={avatarUrl}
-        width={8}
-        height={8}
+        width={18}
+        height={18}
         alt="User avatar"
         className="h-8 w-8"
       />
